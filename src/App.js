d3d16397415d7c5886a11cba6fd7111a73b5ae02
Mohamed-Ziyad <ziyad.web.developer@gmail.com>
2019-03-30 10:16:23 +0530

    All done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+
+const App = () => (<PersonList />) 
+
+const PersonList= ()=>{
+  const People =[
+    {
+      img:22,name:'John',job:'developer'
+    },
+    {
+      img:34,name:'Ann',job:'designer'
+    },
+    {
+      img:56,name:'Saman',job:'Programmer'
+    }
+  ];
+
+  return (<section>
+    <Person person={People[0]}/>;
+    <Person person={People[1]}/>;
+    <Person person={People[2]}/>;
+  </section>)
 }
+
+const Person = (props) => {
+  const {img,name,job}=props.person;
+
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+  
+  
+  
+  return(
+    <div className="person">
+    <img src={url} alt=""/>
+    <div>
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+    </div>
+     </div>
+  )
+}
+
 
 export default App;
